654d8a5ce1b37f9aa59ad7367bda98f352f73681
Chung-Han Hsieh <chhsieh@Chung-Hans-MacBook-Pro-M4-Max-64GB.local>
2026-02-14 10:24:17 +0800

    chore: remove duplicate src/src directory

--- src/src/components/effects/index.js
+++
@@ -1,5 +0,0 @@
-export { default as FireEffect } from './FireEffect';
-export { default as ElecEffect } from './ElecEffect';
-export { default as WaterEffect } from './WaterEffect';
-export { default as GrassEffect } from './GrassEffect';
-export { default as DarkEffect } from './DarkEffect';
